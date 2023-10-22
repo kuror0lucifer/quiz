9d5b62ec349eb5800b78eb34a7b68b1bd2402cbb
kuror0lucifer <kovalishin.2014@mail.ru>
2023-10-22 17:41:56 +0200

    Field

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import Field from "./components/Field";
+
+export default function App() {
+  return (
+    <main>
+      <Field />
+    </main>
+  );
+}
---
+++ src/components/Field.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const questions = [
+  {
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
+    correct: 0,
+  },
+  {
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
+    correct: 1,
+  },
+  {
+    title: "Что такое JSX?",
+    variants: [
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
+    ],
+    correct: 2,
+  },
+];
+
+export default function Field() {
+  return (
+    <div className="wrapper">
+      {questions.map((el, index = 0) => {
+        return <div key={index}>{el.title}</div>;
+      })}
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
